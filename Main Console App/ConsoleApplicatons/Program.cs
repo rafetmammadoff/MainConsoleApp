a930329b175878a11c023b6b8496f8b498475c28
Rafet <professionalraf@gmail.com>
2022-06-29 15:27:32 +0400

    Interface implementation

---
+++ Main Console App/ConsoleApplicatons/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleApplicatons
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> nums = new List<int>()
+            {
+                0,1,2,3
+            };
+            Console.WriteLine(nums.Find(x=>x==0));
+        }
+    }
+}
---
+++ Main Console App/Main Console App/Manager.cs
@@ -0,0 +1,62 @@
+﻿using Main_Console_App.Enums;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Main_Console_App
+{
+    class Manager : ITodoManager
+    {
+        public List<TodoItem> TodoItems => new List<TodoItem>();
+
+        public void AddTodoItem(TodoItem todoItem)
+        {
+            TodoItems.Add(todoItem);
+        }
+
+        public void ChangeTodoItemStatus(int no, TodoStatus status)
+        {
+            TodoItem todoItem = TodoItems.Find(x => x.No == no);
+            todoItem.Status = status;
+
+        }
+
+        public void DeleteTodoItem(int no)
+        {
+            TodoItem todoItem = TodoItems.Find(x => x.No == no);
+            TodoItems.Remove(todoItem);
+        }
+
+        public void EditTodoItem(int no, string tittle, string description, DateTime deadline)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<TodoItem> FilterTodoItems(DateTime fromDate, DateTime toDate, TodoStatus? status)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<TodoItem> GetAllDelayedTasks()
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<TodoItem> GetAllTodoItems()
+        {
+            return TodoItems;
+        }
+
+        public List<TodoItem> GetAllTodoItemsByStatus(TodoStatus status)
+        {
+            List<TodoItem> NewTodoItems = TodoItems.FindAll(x => x.Status == status);
+            return NewTodoItems;
+        }
+
+        public List<TodoItem> SearchTodoItems(string text)
+        {
+            List<TodoItem> NewTodoItems = TodoItems.FindAll(x => x.Tittle.Contains(text));
+            return NewTodoItems;
+        }
+    }
+}
