072efaafd850f938a7fe71decd7b577a42141c85
Rafet <professionalraf@gmail.com>
2022-06-29 14:40:38 +0400

    ITodoManager interface created

---
+++ Main Console App/Main Console App/ITodoManager.cs
@@ -0,0 +1,21 @@
+﻿using Main_Console_App.Enums;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Main_Console_App
+{
+    interface ITodoManager
+    {
+        List<TodoItem> TodoItems { get; }
+        public void AddTodoItem(TodoItem todoItem);
+        public List<TodoItem> GetAllTodoItems();
+        public List<TodoItem> GetAllDelayedTasks();
+        public void ChangeTodoItemStatus(int no,TodoStatus status);
+        public void EditTodoItem(int no,string tittle,string description,DateTime deadline);
+        public void DeleteTodoItem(int no);
+        public List<TodoItem> GetAllTodoItemsByStatus(TodoStatus status);
+        public List<TodoItem> SearchTodoItems(string text);
+        public List<TodoItem> FilterTodoItems(DateTime fromDate,DateTime toDate, TodoStatus? status);
+    }
+}
