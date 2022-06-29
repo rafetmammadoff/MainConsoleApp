4a7aa1f92e48da296226c7d57a8e39cd256ba7d4
Rafet <professionalraf@gmail.com>
2022-06-29 14:24:06 +0400

    Todo Item created

---
+++ Main Console App/Main Console App/Enums/TodoStatus.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Main_Console_App.Enums
+{
+    enum TodoStatus
+    {
+        Todo,
+        Doing,
+        Done
+    }
+}
---
+++ Main Console App/Main Console App/TodoItem.cs
@@ -0,0 +1,25 @@
+﻿using Main_Console_App.Enums;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Main_Console_App
+{
+    class TodoItem
+    {
+        public TodoItem()
+        {
+            _totalCount++;
+            _no = _totalCount;
+        }
+        private int _no;
+        private static int _totalCount;
+        public int No => _no;
+        public string Tittle;
+        public string Description;
+        public TodoStatus Status;
+        public DateTime DeadLine;
+        public DateTime TypeChangeAt;
+
+    }
+}
